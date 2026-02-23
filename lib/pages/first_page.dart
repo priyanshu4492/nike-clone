73806017c3fe5aff2648b5c54b93358b5c1359a2
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-02-23 21:34:11 +0530

    nike clone app

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class firstPage extends StatelessWidget {
+  const firstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      
+    );
+  }
+}
